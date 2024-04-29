91cb03b89448b54a5ec1ada62f5d3f6478165ed5
yashpredator <yashrajsrivastava555@gmail.com>
2024-04-30 02:21:19 +0530

    DB connection successfull

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true, 
+    useUnifiedTopology:true, 
+})
+.then(()=>{ 
+    console.log("DB connection successfull");
+})
+.catch((err)=>{ 
+    console.log("There is error in Db connectin",err.message);
+});
+
 const server=app.listen(process.env.PORT,()=>{
     console.log(`Server started on Port ${process.env.PORT}`);
 });
