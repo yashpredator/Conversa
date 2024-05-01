81f63e8065de79879e2d0a68893956007adb6074
yashpredator <yashrajsrivastava555@gmail.com>
2024-05-02 00:04:46 +0530

    Register login done with response in mongodb

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express=require("express");
 const cors=require("cors");
 const mongoose=require("mongoose");
-
+const UserRoutes=require("../server/routes/UserRoutes");
 const app=express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth",UserRoutes); 
+// app.use("/api/messages",messageRoutes);
+
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true, 
     useUnifiedTopology:true, 
-})
+})  
 .then(()=>{ 
     console.log("DB connection successfull");
 })
