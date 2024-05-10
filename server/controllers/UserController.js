f5100e2e789fb365bb2e24e6bc22fef9406f6da1
yashpredator <yashrajsrivastava555@gmail.com>
2024-05-11 03:19:33 +0530

    Backend

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -60,3 +60,27 @@ module.exports.register = async (req, res, next) => {
       next(ex);
     }
   };
+
+  module.exports.logout = (req, res, next) => {
+    try {
+      if (!req.params.id) return res.json({ msg: "User id is required " });
+      onlineUsers.delete(req.params.id);
+      return res.status(200).send();
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
+  module.exports.getAllUsers = async (req, res, next) => {
+    try {
+      const users = await User.find({ _id: { $ne: req.params.id } }).select([
+        "email",
+        "username",
+        "avatarImage",
+        "_id",
+      ]);
+      return res.json(users);
+    } catch (ex) {
+      next(ex);
+    }
+  }
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,7 +1,10 @@
 
-const {register, login,setavatar}=require("../controllers/UserController");
+const {register, login,setavatar,logout,getAllUsers}=require("../controllers/UserController");
 const router=require("express").Router();
+
 router.post("/register",register);
 router.post("/login",login);
 router.post("/setavatar/:id",setavatar);
+router.get("/logout/:id",logout);
+router.get("/allusers/:id",getAllUsers);
 module.exports=router;
