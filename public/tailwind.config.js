b52027bdbf6fa394175540c851781979081ed20d
yashpredator <140188560+yashpredator@users.noreply.github.com>
2024-06-06 19:37:25 +0530

    Add files via upload

---
+++ public/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
